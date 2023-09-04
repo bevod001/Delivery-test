6cb94e0b44119b996b7f4ae6a2097c1e8c6fe836
bevod001 <98601149+bevod001@users.noreply.github.com>
2023-09-05 02:10:48 +0300

    05/09/2023-2

---
+++ js/dist/script.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".slider__inner").slick({
+    dots: false,
+    infinite: true,
+    speed: 1200,
+    slidesToShow: 3,
+    sliderToScroll: 1,
+    adaptiveHeight: false,
+    centerMode: true,
+    centerPadding: "20px",
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/prev.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next.png"></button>',
+    responsive: [{
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 1
+      }
+    }, {
+      breakpoint: 320,
+      settings: {}
+    }]
+  });
+});
