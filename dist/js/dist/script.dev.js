b86089d17c6d468f02fdd946ec21afbfd391f8e1
bevod001 <98601149+bevod001@users.noreply.github.com>
2023-09-05 02:08:39 +0300

    05/09/2023-1

--- dist/js/dist/script.dev.js
+++ dist/js/dist/script.dev.js
@@ -9,11 +9,20 @@ $(document).ready(function () {
     sliderToScroll: 1,
     adaptiveHeight: false,
     centerMode: true,
-    centerPadding: "50px",
-    autoplay: true,
+    centerPadding: "20px",
+    autoplay: false,
     autoplaySpeed: 2000,
     arrows: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/prev.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next.png"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next.png"></button>',
+    responsive: [{
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 1
+      }
+    }, {
+      breakpoint: 320,
+      settings: {}
+    }]
   });
 });
