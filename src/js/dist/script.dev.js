2f0457b4d304617df88d46a3b32f0769e1736a63
bevod001 <98601149+bevod001@users.noreply.github.com>
2023-09-07 00:35:31 +0300

    07/09/2023-2

--- src/js/dist/script.dev.js
+++ src/js/dist/script.dev.js
@@ -9,11 +9,35 @@ $(document).ready(function () {
     sliderToScroll: 1,
     adaptiveHeight: false,
     centerMode: true,
-    centerPadding: "50px",
-    autoplay: true,
+    centerPadding: "20px",
+    autoplay: false,
     autoplaySpeed: 2000,
     arrows: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/prev.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next.png"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next.png"></button>',
+    responsive: [{
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 1
+      }
+    }, {
+      breakpoint: 320,
+      settings: {}
+    }]
+  });
+});
+window.addEventListener("DOMContentLoaded", function () {
+  var menu = document.querySelector(".promo__menu-nav"),
+      menuItem = document.querySelectorAll(".promo__menu-item"),
+      hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("promo__menu-nav-active");
+  });
+  menuItem.forEach(function (item) {
+    item.addEventListener("click", function () {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("promo__menu-nav-active");
+    });
   });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -7,13 +7,43 @@ $(document).ready(function () {
     sliderToScroll: 1,
     adaptiveHeight: false,
     centerMode: true,
-    centerPadding: "50px",
-    autoplay: true,
+    centerPadding: "20px",
+    autoplay: false,
     autoplaySpeed: 2000,
     arrows: true,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="../icons/slider/prev.png"></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="../icons/slider/next.png"></button>',
+    responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {},
+      },
+    ],
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".promo__menu-nav"),
+    menuItem = document.querySelectorAll(".promo__menu-item"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("promo__menu-nav-active");
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("promo__menu-nav-active");
+    });
   });
 });
